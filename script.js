05be30fe41f742b1a7fcf85464f91edf33ea4935
Aziz5789 <87742299+Aziz5789@users.noreply.github.com>
2021-09-28 19:39:45 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -23,24 +23,50 @@ $(document).ready(function(){
         + "&type=video&part=snippet&maxResults=" +max+ "&q=" + search, function(data){
             console.log(data)
 
-            data.items.forEach(item => {
-            
-                video = `
-                
-                <iframe  width="420" height="315"
-                src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen"
-                 ></iframe> 
-                
-                `
-                $("#videos").append(video)
-                
-            });
+            var videoData = "";
+
+             $("#table").show();
+
+             data.items.forEach((item) => {
+                     videoData = `
+                    
+                    <tr id="${item.id.videoId}">
+                    <td>
+                    ${item.snippet.title}</td>
+                    <td>
+                    <img width="200" height="200" src="${item.snippet.thumbnails.high.url}"/>
+                    </td>
+                    <td>
+                    <a target="_blank" href="https://www.youtube.com/channel/${item.snippet.channelId}">${item.snippet.channelTitle}</a>
+                    </td>
+                    </tr>
+
+                    `;
+
+                  $("#results").append(videoData);
+             });
 
         });
 
         
     }
 
+    $('#results').on('click', 'tr', function() {
+        $("#table").hide();
+        var vidID = $(this).closest('tr').attr('id');
+        video = `
+                
+                <iframe  width="420" height="315"
+                src="https://www.youtube.com/embed/${vidID}" frameborder="0" allowfullscreen"
+                 ></iframe> 
+                
+                `
+                $("#videos").append(video)
+
+       
+    });
+
+    
 })
 
 
